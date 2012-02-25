864e6b54b1149dbfbb1c3c9049bcdce71dde72f0
Pedro Gaspar <pedro.gaxpar@gmail.com>
2012-02-25 16:00:24 +0000

    Admin login now defined on Heroku.

--- app.rb
+++ app.rb
@@ -122,7 +122,7 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['admin', 'connect_cbr!']
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [ENV['ADMIN_USERNAME'] || 'admin', ENV['ADMIN_PASSWORD'] || 'password']
   end
 
 end
